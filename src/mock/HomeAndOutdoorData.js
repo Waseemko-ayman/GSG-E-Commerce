7496eb938bca3b42ca8c877ec308153e43dbb0d2
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-08-26 20:32:29 +0300

    Create mock fiels

---
+++ src/mock/HomeAndOutdoorData.js
@@ -0,0 +1,59 @@
+import HomeAndOutdoor1 from "../../public/assets/HomeAndOutdoor1.png";
+import HomeAndOutdoor2 from "../../public/assets/HomeAndOutdoor2.png";
+import HomeAndOutdoor3 from "../../public/assets/HomeAndOutdoor3.png";
+import HomeAndOutdoor4 from "../../public/assets/HomeAndOutdoor4.png";
+import HomeAndOutdoor5 from "../../public/assets/HomeAndOutdoor5.png";
+import HomeAndOutdoor6 from "../../public/assets/HomeAndOutdoor6.png";
+import HomeAndOutdoor7 from "../../public/assets/HomeAndOutdoor7.png";
+import HomeAndOutdoor8 from "../../public/assets/HomeAndOutdoor8.png";
+
+export const HOME_OUTDOOR_DATA = [
+  {
+    id: 1,
+    src: HomeAndOutdoor1,
+    title: "Soft chairs",
+    price: "19",
+  },
+  {
+    id: 2,
+    src: HomeAndOutdoor2,
+    title: "Sofa & chair",
+    price: "19",
+  },
+  {
+    id: 3,
+    src: HomeAndOutdoor3,
+    title: "Kitchen dishes",
+    price: "19",
+  },
+  {
+    id: 4,
+    src: HomeAndOutdoor5,
+    title: "Smart watches",
+    price: "19",
+  },
+  {
+    id: 5,
+    src: HomeAndOutdoor6,
+    title: "Kitchen mixer",
+    price: "100",
+  },
+  {
+    id: 6,
+    src: HomeAndOutdoor7,
+    title: "Blenders",
+    price: "39",
+  },
+  {
+    id: 7,
+    src: HomeAndOutdoor8,
+    title: "Home appliance",
+    price: "19",
+  },
+  {
+    id: 8,
+    src: HomeAndOutdoor4,
+    title: "Coffee maker",
+    price: "10",
+  },
+];
