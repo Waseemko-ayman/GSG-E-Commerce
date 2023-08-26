bc634be47ca55f41769d9de60af3c0cef11d4588
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-08-26 20:34:46 +0300

    call up Sections in Home Page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,18 @@
 import Container from "@/components/organism/Container";
 import React from "react";
 import Landing from "./Sections/LandingSection";
+import DealsAndOffers from "./Sections/DealsAndOffersSection";
+import HomeAndOutDoor from "./Sections/HomeAndOutDoor";
+import RequestSuppliers from "./Sections/RequestSuppliers";
 
 const HomePage = () => {
   return (
     <main>
       <Container>
         <Landing />
+        <DealsAndOffers />
+        <HomeAndOutDoor />
+        <RequestSuppliers />
       </Container>
     </main>
   );
