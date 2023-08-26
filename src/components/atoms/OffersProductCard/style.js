9ceecfae49bf8dab7d1905160bcff488c4859ddc
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-08-26 20:27:44 +0300

    Build Offers Product Card

---
+++ src/components/atoms/OffersProductCard/style.js
@@ -0,0 +1,28 @@
+import { StyledFlexColumn } from "@/style/common";
+import { styled } from "styled-components";
+
+export const StyledProductCard = styled(StyledFlexColumn)`
+  width: 179px;
+  height: 235px;
+  padding: 20px;
+  border-left: 1px solid var(--gray-300-color);
+  background-color: var(--white-color);
+  cursor: pointer;
+
+  .text {
+    text-align: center;
+  }
+  
+  p {
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: -0.2px;
+    margin: 11px 0 7px;
+  }
+
+  img {
+    max-width: 100%;
+  }
+`;
