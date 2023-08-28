c71c05d5205a398148db3cad7a1924f3b9f7e643
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-08-28 04:44:20 +0300

    move  footer styling to style.js

--- src/components/organism/Footer/index.jsx
+++ src/components/organism/Footer/index.jsx
@@ -13,45 +13,7 @@ import {
   INFORMATION_LIST,
   PARTNERSHIP_LIST,
 } from "@/mock/FooterLists";
-
-const StyledFooter = styled(StyledFlex)`
-  background-color: var(--white-color);
-  padding: 40px 20px 60px;
-`;
-
-const StyledFooterContent = styled(StyledFlex)`
-  display: grid;
-  grid-template-columns: 300px 1fr;
-  gap: 25px;
-
-  .info {
-  }
-
-  .info p {
-    color: var(--gray-600-color);
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 24px;
-    letter-spacing: -0.2px;
-    margin: 16px 0 15px;
-  }
-
-  h3 {
-    color: var(--dark-color);
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 22px;
-    margin-bottom: 10px;
-  }
-
-  img {
-    display: block;
-    margin-bottom: 8px;
-    cursor: pointer;
-  }
-`;
+import { StyledFooter, StyledFooterContent } from "./style";
 
 const Footer = () => {
   return (
