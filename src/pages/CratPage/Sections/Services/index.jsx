71e5807948f0c2fe19d24980d1b13a560e114ab6
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-08-29 16:32:05 +0300

    update services section

--- src/pages/CratPage/Sections/Services/index.jsx
+++ src/pages/CratPage/Sections/Services/index.jsx
@@ -1,14 +1,14 @@
 import ServicesCard from '@/components/molecules/ServicesCard';
 import { SERVICES_DATA } from '@/mock/ServicesCardData';
-import { StyledServices } from './style';
+import { StyledAlignFlex } from '@/style/common';
 
 const Services = () => {
   return (
-    <StyledServices gap="54px" margin="20px 0">
+    <StyledAlignFlex gap="54px" margin="20px 0">
       {SERVICES_DATA.map(({ id, src, title, decription })=> (
         <ServicesCard key={id} imageSrc={src} titleService={title} decription={decription} />
       ))}
-    </StyledServices>
+    </StyledAlignFlex>
   )
 }
 
