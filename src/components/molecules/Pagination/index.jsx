c39f0a1164f8ea2d8805ff4c137af540b75cf698
htahrawi <h.tahrawi01@gmail.com>
2023-09-04 01:26:29 +0300

    make the pagination with sizes

--- src/components/molecules/Pagination/index.jsx
+++ src/components/molecules/Pagination/index.jsx
@@ -6,52 +6,44 @@ import { StyledPagination } from "./style";
 import useAPI from "@/hook/useAPI";
 import { API_URL } from "@/config/api";
 import { useEffect, useState } from "react";
+import { getProducts } from "@/redux/slices/products";
+import { useDispatch, useSelector } from "react-redux";
 
 const Pagination = () => {
   const [num, setNum] = useState(1);
   const [size, setSize] = useState(10);
-  
-  // const config = {
-  //   headers: { Authorization: `Bearer ${token}` },
-  // };
 
   const pageQuery = `?_page=${num}`;
-  // const searchQuery = search ? `&q=${deferredQuery}` : '';
   const sizeQuery = `&_limit=${size}`;
 
-  // const { getUsers, users, deleteUser } = useAPI(
-  //   AUTH_API_URL + "users" + sizeQuery + searchQuery + pageQuery,
-  //   config
+  // const { getUsers, users } = useAPI(
+  //   API_URL + "products" + pageQuery + sizeQuery
   // );
-  const { getUsers, users } = useAPI(
-    API_URL + "products" + pageQuery + sizeQuery
-  );
-
+  const { products } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
   const prevSubmit = () => {
     setNum((prev) => (num > 1 ? prev - 1 : num));
   };
   const nextSubmit = () => {
-    setNum((prev) => (num <= users.pages ? prev + 1 : num));
+    setNum((prev) => (num < Math.ceil(products.length / size) ? prev + 1 : num));  
   };
 
   useEffect(() => {
-    getUsers();
+    dispatch(getProducts())
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [size, num]);
+  }, []);
 
   return (
     <StyledPagination gap="9px">
       <div className="selects">
-        <select onChange={(e) => setSize(e.target.value)} name='size'>
-          <option value="10" selected>10</option>
-          <option value="20">20</option>
-          <option value="30">30</option>
-          <option value="40">40</option>
-          <option value="50">50</option>
+        <select onChange={(e) => { setSize(e.target.value); console.log(size); }} name='size'>
+          <option value="6" selected>6</option>
+          <option value="8">8</option>
+          <option value="10">10</option>
         </select>
       </div>
       <StyledAlignFlex>
-        <Button
+        {/* <Button
           color="dark"
           variant="secondary"
           borderColor="gray"
@@ -59,12 +51,13 @@ const Pagination = () => {
           imageSrc="/assets/chevron-left.svg"
           onClick={prevSubmit} 
           disabled={users.page === 1 ? true : false}
-        />
-        <button>{users.page}</button>
+        /> */}
+        <button onClick={prevSubmit}>-</button>
+        <button>{num > Math.ceil(products.length / size) ? Math.ceil(products.length / size) : num}</button>
         {/* <Button color="dark" variant="secondary" text="1" borderColor="gray" padding="10px" imageHidden />
         <Button color="dark" variant="secondary" text="2" borderColor="gray" padding="10px" imageHidden />
         <Button color="dark" variant="secondary" text="3" borderColor="gray" padding="10px" imageHidden /> */}
-        <Button
+        {/* <Button
           color="dark"
           variant="secondary"
           borderColor="gray"
@@ -72,7 +65,9 @@ const Pagination = () => {
           imageSrc="/assets/chevron_right.svg"
           onClick={nextSubmit} 
           disabled={users.page === users.pages ? true : false}
-        />
+        /> */}
+
+        <button onClick={nextSubmit}>+</button>
       </StyledAlignFlex>
     </StyledPagination>
   );
