ed0f0e4ffc2c40e41963e3d26bbb0edf09162f9a
htahrawi <h.tahrawi01@gmail.com>
2023-09-09 03:57:55 +0300

    fix joining login page without logout

--- src/app/(guest)/layout.js
+++ src/app/(guest)/layout.js
@@ -1,11 +1,31 @@
+"use client"
 import FooterRegister from "@/components/molecules/FooterRegister";
+import { PATHS } from "@/constants/path";
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from "react";
 
 export default function LayoutAuth({ children }) {
+  const router = useRouter();
+  const [isAuth, setIsAuth] = useState(true)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const auth = localStorage.getItem('isAuth')
+      setIsAuth(auth)
+      console.log("AAAA ", auth);
+    }
+  }, [])
   return (
     <>
       <div>
-        {children}
-        <FooterRegister />
+        {
+          !isAuth ? 
+          <>
+            {children}
+            <FooterRegister />
+          </>
+          : 
+          router.push(PATHS.HOME)
+        }
       </div>
     </>
   );
